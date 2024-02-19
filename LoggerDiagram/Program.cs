58585734fcefe70535d9ea05b133d3cf2848ab8c
Onixfff <danilnikonov01@mail.ru>
2024-02-19 08:38:10 +0200

    test

--- LoggerDiagram/Program.cs
+++ LoggerDiagram/Program.cs
@@ -8,13 +8,13 @@ namespace LoggerDiagram
         static void Main(string[] args)
         {
             PLCConnector plc1= new PLCConnector("192.168.37.104");
-            PLCConnector plc1= new PLCConnector();
-            PLCConnector plc2 = new PLCConnector("192.168.37.104");
+            PLCConnector plc2 = new PLCConnector("192.168.37.103");
             while (true)
             {
                 try
                 {
-                    
+                    plc1.TryTakesData();
+                    plc2.TryTakesData();
                 }
                 catch (Exception ex)
                 {
