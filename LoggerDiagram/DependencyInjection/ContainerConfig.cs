ee86c37481a5f9172d499ea9b7fe8c0fe4d5029c
Onixfff <danilnikonov01@mail.ru>
2025-04-15 18:02:52 +0200

    Добавил в проект Autofac

---
+++ LoggerDiagram/DependencyInjection/ContainerConfig.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+
+namespace LoggerDiagram.DependencyInjection
+{
+    internal class ContainerConfig
+    {
+        public IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            //builder.RegisterType<>().As<>();
+
+            return builder.Build();
+        }
+    }
+}
