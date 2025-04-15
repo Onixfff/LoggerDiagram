88b041069656a7b471960908a05f6cfddc3c2cbb
Onixfff <danilnikonov01@mail.ru>
2025-04-15 17:58:44 +0200

    Создал модели данных бд

---
+++ LoggerDiagram/Models/DataPoints.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoggerDiagram.Models
+{
+    internal class DataPoints
+    {
+        public readonly int BatchNumber;
+        public readonly DateTime NowTime;
+        public readonly float Value;
+        public readonly int Time;
+
+        private DataPoints(int batchNumber, DateTime nowTime, float value, int time)
+        {
+            BatchNumber = batchNumber;
+            NowTime = nowTime;
+            Value = value;
+            Time = time;
+        }
+
+        public static DataPoints Create(int batchNumber, DateTime nowTime, float value, int time)
+        {
+            int maxGraphId = 30;
+
+            //Проверка данных
+            if(batchNumber < 0 && batchNumber > maxGraphId)
+            {
+                throw new ArgumentOutOfRangeException("Номер партии вышел за допустимый диапозон");
+            }
+
+            return new DataPoints(batchNumber, nowTime, value, time);
+        }
+    }
+}
---
+++ LoggerDiagram/Models/Graph.cs
@@ -0,0 +1,20 @@
+﻿namespace LoggerDiagram.Models
+{
+    internal class Graph
+    {
+        private readonly int Id;
+        private readonly string Comment;
+
+        private Graph(int id, string comment)
+        {
+            Id = id;
+            Comment = comment;
+        }
+
+        public static Graph Create(int id, string comment)
+        {
+            var graph = new Graph(id, comment);
+            return graph;
+        }
+    }
+}
