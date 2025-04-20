47033bed9dae69533674704ca7308682ec6aeda9
Onixfff <danilnikonov01@mail.ru>
2025-04-20 19:42:54 +0200

    Сделал Program для запуска проекта

--- LoggerDiagram/Application/ApplicationService.cs
+++ LoggerDiagram/Application/ApplicationService.cs
@@ -1,8 +1,10 @@
 ﻿using LoggerDiagram.DataAccess;
+using LoggerDiagram.DTO;
 using LoggerDiagram.Models.Plc;
 using LoggerDiagram.Services;
 using NLog;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,6 +16,10 @@ namespace LoggerDiagram.Application
         private readonly IDataBaseRepository _dataBaseRepository;
         private readonly ILogger _logger;
 
+        private const int StartIndex1 = 1;
+        private const int StartIndex2 = 0;
+        private const int MaxIndex = 38;
+
         public ApplicationService(IPlcDataReaderFactory plcDataReaderFactory, IDataBaseRepository dataBaseRepository, ILogger logger)
         {
             _plcDataReaderFactory = plcDataReaderFactory;
@@ -23,25 +29,32 @@ namespace LoggerDiagram.Application
 
         public async Task RunAsync(CancellationToken token)
         {
-
             IPlcDataReader plcDataReader1 = _plcDataReaderFactory.Create("");
             IPlcDataReader plcDataReader2 = _plcDataReaderFactory.Create("");
 
-            while (true)
+            while (token.IsCancellationRequested)
             {
                 try
                 {
-                    int byteCount = 0;
-                    int doubleCount = 2;
-                    int timeCount = 6;
 
-                    for (int i = 0; i < 18; i++)
+                    //Получение данных
+                    var tasks = new[]
                     {
-                        PlcLogEntry plcLogEntity = await _plcDataReader.GetDataAsync(byteCount, doubleCount, timeCount, token);
+                        ProcessReaderAsync(StartIndex1, MaxIndex, plcDataReader1, nameof(plcDataReader1), token),
+                        ProcessReaderAsync(StartIndex2, MaxIndex, plcDataReader2, nameof(plcDataReader2), token)
+                    };
 
-                        byteCount += 8;
-                        doubleCount += 8;
-                        timeCount += 8;
+                    var results = await Task.WhenAll(tasks);
+                    List<PlcLogEntryDto> result1 = results[0];
+                    List<PlcLogEntryDto> result2 = results[1];
+                    
+                    //TODO Перебор данных до отправки (если 0 или 1 в байтах) нужно добавить логику возможно её стоит добавить в модель при создании чтобы он сам увеличивал значение на 1 в batchNunber
+
+                    //Отправка данных
+                    for (int i = 0; i < MaxIndex/2; i++)
+                    {
+                        await ProcessSendDataAsync(result1[i], token);
+                        await ProcessSendDataAsync(result2[i], token);
                     }
                 }
                 catch (Exception ex)
@@ -49,6 +62,46 @@ namespace LoggerDiagram.Application
                     _logger.Error(ex, "Поймана не ведомая ошибка");
                 }
             }
+        }
+
+        private async Task ProcessSendDataAsync(PlcLogEntryDto plcLogEntryDto ,CancellationToken token)
+        {
+            await _dataBaseRepository.SendDataAsync(plcLogEntryDto, token);
+            _logger.Info("Отправил данные");
+        }
+
+        private async Task<List<PlcLogEntryDto>> ProcessReaderAsync(int startI, int maxI, IPlcDataReader plcDataReader, string readerName, CancellationToken token)
+        {
+            var result = await Get(startI, maxI, plcDataReader, token);
+            _logger.Info($"Получил данные для {readerName}");
+            return result;
+        }
+
+        private async Task<List<PlcLogEntryDto>> Get(int startI, int maxI, IPlcDataReader plcDataReader, CancellationToken token)
+        {
+            List<PlcLogEntry> plcLogEntitys = new List<PlcLogEntry>();
+            List<PlcLogEntryDto> plcLogEntryDtos = new List<PlcLogEntryDto>();
+
+            int byteCount = 0;
+            int doubleCount = 2;
+            int timeCount = 6;
+
+            for(int i = startI; i < maxI/2; i+=2)
+            {
+                plcLogEntitys.Add(await plcDataReader.GetDataAsync(byteCount, doubleCount, timeCount, token));
+                plcLogEntryDtos.Add(ConvertPlcLogEntityInDto(i, plcLogEntitys[i]));
+
+                byteCount += 8;
+                doubleCount += 8;
+                timeCount += 8;
+            }
+
+            return plcLogEntryDtos;
+        }
+
+        private PlcLogEntryDto ConvertPlcLogEntityInDto(int id, PlcLogEntry plcLogEntry)
+        {
+            return PlcLogEntryDto.Create(id, plcLogEntry.Value, plcLogEntry.Time);
         }
     }
 }
--- LoggerDiagram/Program.cs
+++ LoggerDiagram/Program.cs
@@ -1,14 +1,39 @@
 ﻿using Autofac;
+using LoggerDiagram.Application;
 using LoggerDiagram.DependencyInjection;
+using NLog;
+using System;
+using System.Threading;
 
 namespace LoggerDiagram
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
             ContainerConfig containerConfig = new ContainerConfig();
             IContainer container = containerConfig.Configure();
+
+            using(var score = container.BeginLifetimeScope())
+            {
+                var application = score.Resolve<ApplicationService>();
+                var logger = score.Resolve<ILogger>();
+
+                var cts = new CancellationTokenSource();
+
+                try
+                {
+                    await application.RunAsync(cts.Token);
+                }
+                catch (OperationCanceledException ex)
+                {
+                    logger.Error(ex,"Операция была отменена.");
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex, "Неизвестая ошибка.");
+                }
+            }
         }
     }
 }
