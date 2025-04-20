8adab45e52b7b60f8fad2a4b55ea990b7d65758b
Onixfff <danilnikonov01@mail.ru>
2025-04-20 10:47:40 +0200

    Убрал using а то не очень удобно смотреть

--- LoggerDiagram/db/DataBase.cs
+++ LoggerDiagram/db/DataBase.cs
@@ -1,14 +1,6 @@
-﻿using LoggerDiagram.Plc;
-using MySql.Data.MySqlClient;
-using Mysqlx.Session;
-using NLog;
+﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LoggerDiagram.DB
 {
