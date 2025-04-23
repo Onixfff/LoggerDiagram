86a77f7699e46150dca9dc1f41ba09e2994e51c4
Onixfff <danilnikonov01@mail.ru>
2025-04-24 00:29:19 +0200

    Делаю тест бд

---
+++ LoggerDiagram.Test/GetLastBatchNumberByGraphAsync.cs
@@ -0,0 +1,30 @@
+﻿using LoggerDiagram.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[TestClass]
+public class GetLastBatchNumberByGraphAsync
+{
+    [TestMethod]
+    public async Task GetAllSteamFlowAsync_ShouldReturnDataFromDatabase()
+    {
+        // Arrange
+        var connectionString = "Database=spslogger; Server=localhost; port=3306; username=root; password=12345; charset=utf8";
+        DataBaseRepository dataBaseServices = new DataBaseRepository(connectionString);
+
+        // Act
+        List<SteamFlow> result = null;
+        await Task.Run(async () =>
+        {
+            result = await dataBaseServices.GetAllSteamFlowAsync();
+        });
+
+        // Assert
+        Assert.IsNotNull(result); // Проверяем, что результат не null
+        Assert.IsTrue(result.Count > 0); // Проверяем, что список не пустой
+        Assert.AreEqual(37810, result.Count); // Проверяем количество записей
+        Assert.AreEqual(1, result[0].Id); // Проверяем первую запись
+        Assert.AreEqual(0, result[0].Data_672); // Проверяем значение Data_672
+    }
+}
---
+++ LoggerDiagram.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LoggerDiagram.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggerDiagram.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d9ca7f5-fa51-43ff-a21f-788661198f52")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
