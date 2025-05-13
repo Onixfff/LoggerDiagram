c935fcd15a073cb231a5107b7da113937275d081
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-13 17:12:28 +0200

    Исправил ошибку

--- LoggerDiagram/DependencyInjection/ContainerConfig.cs
+++ LoggerDiagram/DependencyInjection/ContainerConfig.cs
@@ -66,7 +66,7 @@ namespace LoggerDiagram.DependencyInjection
                 .InstancePerLifetimeScope();
 
             builder.RegisterType<InMemoryGraphStateManager>()
-                .As<InMemoryGraphStateManager>()
+                .As<IGraphStateManager>()
                 .SingleInstance();
 
             return builder.Build();
