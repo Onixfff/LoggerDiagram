129b3eec27be769a5e1ad20029ff167aeeca6ffb
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-15 11:38:47 +0200

    Заменил PlcLogEntity и названия её переменных а то они не отражали суть хранящихся данных

---
+++ LoggerDiagram/Models/Plc/PlcSensorReading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LoggerDiagram.Models.Plc
+{
+    public class PlcSensorReading
+    {
+        public Byte StatusByte;
+        public Byte? RoomNumber { get; private set; }
+        public double Value;
+        public short Time;
+
+        private PlcSensorReading(byte statusByte, byte roomNumber, double value, short time)
+        {
+            StatusByte = statusByte;
+            RoomNumber = roomNumber;
+            Value = value;
+            Time = time;
+        }
+
+        public static PlcSensorReading Create(byte rawByteValue, byte rawByteRoom, double value, short time)
+        {
+            //TODO Проверки данных добавить
+            return new PlcSensorReading(rawByteValue, rawByteRoom, value, time);
+        }
+    }
+}
