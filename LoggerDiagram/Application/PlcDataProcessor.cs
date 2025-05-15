a6f010cf57be61c933f87efa513b4139fc1670cc
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-15 14:06:06 +0200

    Вырезал лишний функционал

--- LoggerDiagram/Application/PlcDataProcessor.cs
+++ LoggerDiagram/Application/PlcDataProcessor.cs
@@ -16,17 +16,15 @@ namespace LoggerDiagram.Application
         private readonly IPlcReaderService _readerService;
         private readonly IPlcDataConverter _converter;
         private readonly IPlcDataSender _sender;
-        private readonly IGraphIdSplitter _splitter;
         private readonly IPlcDataReaderFactory _readerFactory;
         private readonly ILogger _logger;
 
-        public PlcDataProcessor(IDataBaseRepository repository,IPlcReaderService readerService, IPlcDataConverter converter, IPlcDataSender sender, IGraphIdSplitter splitter, IPlcDataReaderFactory readerFactory, ILogger logger)
+        public PlcDataProcessor(IDataBaseRepository repository,IPlcReaderService readerService, IPlcDataConverter converter, IPlcDataSender sender, IPlcDataReaderFactory readerFactory, ILogger logger)
         {
             _repository = repository;
             _readerService = readerService;
             _converter = converter;
             _sender = sender;
-            _splitter = splitter;
             _readerFactory = readerFactory;
             _logger = logger;
         }   
@@ -38,9 +36,8 @@ namespace LoggerDiagram.Application
             ValidateIps(ipEven);
 
             var allIds = await GetAllGraphIdsAsync(token);
-            var (evenIds, oddIds) = _splitter.Split(allIds);
 
-            await ProcessGroupAsync(evenIds, ipEven, token);
+            await ProcessGroupAsync(allIds, ipEven, token);
         }
 
         private void ValidateIps(string ipEven)
