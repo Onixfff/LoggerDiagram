1db872d54b53b9da4758201aad12c16ccacc16b9
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-15 15:36:57 +0200

    Начал менять PlcDataConverter добавлять описание и доп проверки

--- LoggerDiagram/Services/PlcDataConverter.cs
+++ LoggerDiagram/Services/PlcDataConverter.cs
@@ -1,6 +1,7 @@
 ﻿using LoggerDiagram.DataAccess;
 using LoggerDiagram.DTO;
 using LoggerDiagram.Models.Plc;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -10,33 +11,51 @@ namespace LoggerDiagram.Services
 {
     public class PlcDataConverter : IPlcDataConverter
     {
+        private readonly ILogger _logger;
         private readonly IDataBaseRepository _repository;
         private readonly IBatchNumberAdjuster _batchAdjuster;
 
-        public PlcDataConverter(IDataBaseRepository repository, IBatchNumberAdjuster batchAdjuster)
+        public PlcDataConverter(IDataBaseRepository repository, IBatchNumberAdjuster batchAdjuster, ILogger logger)
         {
             _repository = repository;
             _batchAdjuster = batchAdjuster;
+            _logger = logger;
         }
 
         public async Task<List<PlcLogEntityDto>> ConvertAsync(List<int> ids, List<PlcSensorReading> entity, CancellationToken token)
         {
-            if (ids.Count != entity.Count)
-                throw new ArgumentException("Кол-во id и данных не совпадает");
+            if (ids == null || entity == null)
+                throw new ArgumentNullException("Параметры ids или entity не могут быть null");
 
             var dtos = new List<PlcLogEntityDto>();
 
-            for(int i = 0; i < ids.Count; i++)
-            {
-                int batch = await _repository.GetLastBatchNumberByGraphAsync(ids[i], token);
-                batch = _batchAdjuster.Adjust(ids[i], batch, entity[i]);
+            int count = Math.Min(ids.Count, entity.Count);
 
-                if (entity[i].Status == 0)
+            if (ids.Count != entity.Count)
+            {
+                _logger.Warn($"Кол-во данных {nameof(ids)} - {ids.Count} != {nameof(entity)} - {entity.Count}. Обработано: {count}");
+            }
+
+            for(int i = 0; i < count; i++)
+            {
+                try
                 {
-                    continue;
+                    int batch = await _repository.GetLastBatchNumberByGraphAsync(ids[i], token);
+                    batch = _batchAdjuster.Adjust(ids[i], batch, entity[i]);
+
+                    if (entity[i].Status == 0)
+                    {
+                        continue;
+                    }
+
+                    dtos.Add(PlcLogEntityDto.Create((int)entity[i].RoomNumber, batch, entity[i].Status, entity[i].Value, entity[i].Time));
+
+                }
+                catch
+                {
+
                 }
 
-                dtos.Add(PlcLogEntityDto.Create((int)entity[i].RoomNumber, batch, entity[i].Status, entity[i].Value, entity[i].Time));
             }
 
             return dtos;
