92c90244a056499ca2dbaa23ebbc14cd00f62c63
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-15 11:30:49 +0200

    Добавил ошибку связанную только с чтением данных из PLC

---
+++ LoggerDiagram/PlcException/PlcDataReadException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LoggerDiagram.PlcException
+{
+    public class PlcDataReadException : Exception
+    {
+        public PlcDataReadException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
