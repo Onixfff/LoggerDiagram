942e01da617b11f583b681e8f671fe7839b4393e
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-15 11:32:20 +0200

    Добавил доп значение RawByteRoom которое хранит номер комнаты из plc с которого мы считываем данные.

--- LoggerDiagram/Models/Plc/PlcLogEntity.cs
+++ LoggerDiagram/Models/Plc/PlcLogEntity.cs
@@ -4,9 +4,10 @@ namespace LoggerDiagram.Models.Plc
 {
     public class PlcLogEntity
     {
-        public readonly Byte RawByteValue;
-        public readonly double Value;
-        public readonly int Time;
+        public Byte RawByteValue;
+        public Byte? RawByteRoom { get; private set; }
+        public double Value;
+        public int Time;
 
         private PlcLogEntity(byte rawByteValue, double value, int time)
         {
@@ -15,11 +16,24 @@ namespace LoggerDiagram.Models.Plc
             Time = time;
         }
 
+        private PlcLogEntity(byte rawByteValue, byte rawByteValueRoom, double value, int time)
+        {
+            RawByteValue = rawByteValue;
+            RawByteRoom = rawByteValueRoom;
+            Value = value;
+            Time = time;
+        }
+
         public static PlcLogEntity Create(byte rawByteValue, double value, int time)
         {
-            //Проверки
-
+            //TODO Проверки данных добавить
             return new PlcLogEntity(rawByteValue, value, time);
+        }
+
+        public static PlcLogEntity Create(byte rawByteValue, byte rawByteRoom, double value, int time)
+        {
+            //TODO Проверки данных добавить
+            return new PlcLogEntity(rawByteValue, rawByteRoom, value, time);
         }
     }
 }
