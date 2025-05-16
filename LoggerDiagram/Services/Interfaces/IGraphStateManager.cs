51ef4e95d798bbd23f07aa80db825c04a64e4c2c
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-16 13:54:56 +0200

    Добавил коментарии

--- LoggerDiagram/Services/Interfaces/IGraphStateManager.cs
+++ LoggerDiagram/Services/Interfaces/IGraphStateManager.cs
@@ -7,7 +7,20 @@ namespace LoggerDiagram.Services.Interfaces
     /// </summary>
     public interface IGraphStateManager
     {
+        /// <summary>
+        /// Получает текущее состояние графика по его идентификатору. 
+        /// Если состояние отсутствует, создается новое.
+        /// </summary>
+        /// <param name="graphId">Идентификатор графика.</param>
+        /// <returns>Текущее состояние графика.</returns>
         GraphState GetState(int graphId);
+        
+        /// <summary>
+        /// Обновляет состояние указанного графика.
+        /// </summary>
+        /// <param name="graphId">Идентификатор графика.</param>
+        /// <param name="state">Новое состояние графика.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         void UpdateState(int graphId, GraphState state);
     }
 }
