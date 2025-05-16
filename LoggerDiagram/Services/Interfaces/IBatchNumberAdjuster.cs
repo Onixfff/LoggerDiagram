71c567dda4d548041fc253ff15938ada3707be23
Onixfff <60624718+Onixfff@users.noreply.github.com>
2025-05-16 13:56:47 +0200

    Добавил коментарий

--- LoggerDiagram/Services/Interfaces/IBatchNumberAdjuster.cs
+++ LoggerDiagram/Services/Interfaces/IBatchNumberAdjuster.cs
@@ -1,9 +1,19 @@
-﻿using LoggerDiagram.Models.Plc;
+﻿using System;
+using LoggerDiagram.Models.Plc;
 
 namespace LoggerDiagram.Services.Interfaces
 {
     public interface IBatchNumberAdjuster
     {
+        /// <summary>
+        /// Корректирует номер партии в зависимости от изменения статуса продукта.
+        /// Если статус изменился с любого другого на ProductState.ZeroProduct, то номер партии увеличивается на 1.
+        /// </summary>
+        /// <param name="graphId">Идентификатор графика.</param>
+        /// <param name="lastBatchNumber">Последний известный номер партии.</param>
+        /// <param name="entity">Объект с данными датчика ПЛК.</param>
+        /// <returns>Обновлённый номер партии.</returns>
+        /// <exception cref="ArgumentNullException">Если параметр <paramref name="entity"/> равен null.</exception>
         int Adjust(int graphId, int lastBatchNumber, PlcSensorReading entity);
     }
 }
